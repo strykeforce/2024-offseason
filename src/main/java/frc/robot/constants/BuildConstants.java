61217e26d269b2dbeaeceb340bd1453b326817d2
Marisa Witcpalek <35316453+mwitcpalek@users.noreply.github.com>
2024-07-23 20:05:46 -0400

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.constants;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024-offseason";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "4362264a8e5b0350e4564fb1d018f12c57c5d189";
+  public static final String GIT_DATE = "2024-07-18 20:23:09 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-07-18 20:23:21 EDT";
+  public static final long BUILD_UNIX_TIME = 1721348601471L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}

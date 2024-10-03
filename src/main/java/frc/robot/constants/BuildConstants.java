952eae5dd89c57e92c8660b3814ca04c444a35cb
KaydenLee456 <klee78982@gmail.com>
2024-10-03 19:01:18 -0400

    Initial Exiter Subsystem

--- src/main/java/frc/robot/constants/BuildConstants.java
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-offseason";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "4362264a8e5b0350e4564fb1d018f12c57c5d189";
-  public static final String GIT_DATE = "2024-07-18 20:23:09 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-07-18 20:23:21 EDT";
-  public static final long BUILD_UNIX_TIME = 1721348601471L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "61217e26d269b2dbeaeceb340bd1453b326817d2";
+  public static final String GIT_DATE = "2024-07-23 20:05:46 EDT";
+  public static final String GIT_BRANCH = "Exiter";
+  public static final String BUILD_DATE = "2024-09-10 19:06:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1726009592688L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }

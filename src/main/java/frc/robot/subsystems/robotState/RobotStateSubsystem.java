6c424f74a0c6b7ff5da9da1a27ab9d416bcf7140
KaydenLee456 <klee78982@gmail.com>
2024-10-08 18:39:53 -0400

    Robot state subsystem setup

---
+++ src/main/java/frc/robot/subsystems/robotState/RobotStateSubsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.robotState;
+
+import frc.robot.subsystems.Intake.*;
+import frc.robot.subsystems.drive.*;;
+
+public class RobotStateSubsystem extends {
+    //Constructor
+    public static RobotStateSubsystem()
+}
